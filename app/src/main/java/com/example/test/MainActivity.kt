20edf160c164371c36a6d8d21d2c843bd59de7a9
kimtaehyuk1 <98taehyukkim@naver.com>
2020-07-09 01:38:16 +0900

    수정했다치고

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {  //onCreate는 앱 최초실행됬을때 수행.
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // xml 화면 뷰를 연결한다.
+
+        tv_title.setText("김배준") // xml 텍스트 id(tv_title)에 있는 내용 바꾸기 setText는 내용 넣는다
     }
+
+
+
 
 }
